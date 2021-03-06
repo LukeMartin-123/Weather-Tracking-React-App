8489ccc17a0a0d0a0a42920328414e27ddfa46ca
Luke Martin <ljmart02@gmail.com>
2021-03-06 12:52:46 -0700

    Connection made to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 
+// Enabling the .env file to be read
+dotenv.config();
+
+// Setting Up Server
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,10 +17,24 @@ app.get("/test", (req, res) => {
     res.send("It Works")
 })
 
+//Connecting to MongoDB
+mongoose.connect(process.env.MDB_CONNECT, 
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+    }, 
+(err) => {
+    if (err) return console.error(err)
+    console.log("Connected to MongoDB")
+});
+
+//Server Connection and Confirmation
 app.listen(PORT, function () {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
 
 });
+
+
 
 
 module.exports = app;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from "react"
 
 function App() {
-  return 
-    <>
-      App
-    </>
-}
+  return (
+    <div>
+      <p> test </p>
+    </div>
+  )}
 
 export default App;

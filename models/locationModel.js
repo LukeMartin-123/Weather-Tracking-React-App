ddfc6b44f8b19ebefe3747cd439aabda24f06663
Luke Martin <ljmart02@gmail.com>
2021-03-06 13:00:05 -0700

    Mongoose Location Model created

---
+++ models/locationModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// Creating Location Schema
+const locationSchema = new mongoose.Schema({
+    city: {type: String, required: true},
+    state: {type: String},
+    country: {type: String, required: true},
+    temperature: {type: Number. required: true}
+});
+
+// Defining the Location Model based on above Schema
+const Location = mongoose.model("location", locationSchema)
+
+module.exports = Location;
